3f029f5f7795bc9bc6ad8cedd6af39a2ecab2855
cricri777 <christian.bruneau777@gmail.com>
2023-06-06 21:15:45 -0400

    looking for a good job title to increase my salary
    - according to kaggle dataset, all time high is Data scientist position in 2021...
    - maybe I should start learning ML then...

--- datascience_salary_2023/queries/queries.sql
+++ datascience_salary_2023/queries/queries.sql
@@ -12,3 +12,24 @@ SELECT
 FROM "CRICRI_DB".kaggle.datascience_salary_2023
 GROUP BY job_title, work_year
 ORDER BY work_year, average_salary DESC;
+
+
+-- looking for all time high salary per job_title to see what I missed as a poor data engineer
+WITH CTE AS (
+    SELECT
+        job_title,
+        salary,
+        work_year,
+        company_location,
+        row_number() over (PARTITION BY job_title ORDER BY salary DESC) ROW_NUM_SALARY_PER_JOB_TITLE
+    FROM "CRICRI_DB".kaggle.datascience_salary_2023
+)
+    SELECT
+        job_title,
+        salary,
+        work_year,
+        company_location,
+        NTILE (5) OVER (ORDER BY salary DESC) bracket
+FROM CTE
+    WHERE ROW_NUM_SALARY_PER_JOB_TITLE = 1
+    ORDER BY salary DESC;
